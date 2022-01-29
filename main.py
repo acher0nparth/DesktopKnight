edee12506a4c93b753e05abf4c2fee6ea1ca34d9
Blake Thacker <62034641+acher0nparth@users.noreply.github.com>
2022-01-28 21:33:32 -0500

    added dynamic image scaling and 32 frames

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import tkinter as tk
 import time
 import random
 
-class pet():
+
+class pet:
     def __init__(self):
         # create a window
         self.window = tk.Tk()
 
         # placeholder image
-        self.walking_right = [tk.PhotoImage(file='DesktopKnight/Images/knight_drinking.gif', format='gif -index %i' % (i)) for i in range(4)]
+        self.walking_right = [
+            tk.PhotoImage(
+                file="Images/knight_drinking.gif", format="gif -index %i" % (i)
+            )
+            for i in range(31)
+        ]
         self.frame_index = 0
         self.img = self.walking_right[self.frame_index]
 
@@ -16,23 +22,30 @@ class pet():
         self.timestamp = time.time()
 
         # set focushighlight to black when the window does not have focus
-        self.window.config(highlightbackground='black')
+        self.window.config(highlightbackground="black")
 
         # make window frameless
         self.window.overrideredirect(True)
 
         # make window draw over all others
-        self.window.attributes('-topmost', True)
+        self.window.attributes("-topmost", True)
 
         # turn black into transparency
-        self.window.wm_attributes('-transparentcolor', 'black')
+        self.window.wm_attributes("-transparentcolor", "black")
 
         # create a label as a container for our image
-        self.label = tk.Label(self.window, bd=0, bg='black')
+        self.label = tk.Label(self.window, bd=0, bg="black")
 
         # create a window of size 128x128 pixels, at coordinates 0,0
         self.x = 0
-        self.window.geometry('64x64+{x}+0'.format(x=str(self.x)))
+        dim = self.walking_right[0].height() * self.walking_right[0].width()
+        self.window.geometry(
+            "{w}x{h}+{x}+0".format(
+                x=str(self.x),
+                w=self.walking_right[0].width(),
+                h=self.walking_right[0].height(),
+            )
+        )
 
         # add the image to our label
         self.label.configure(image=self.img)
@@ -52,11 +65,17 @@ class pet():
         if time.time() > self.timestamp + 0.05:
             self.timestamp = time.time()
             # advance the frame by one, wrap back to 0 at the end
-            self.frame_index = (self.frame_index + 1) % 4
+            self.frame_index = (self.frame_index + 1) % 31
             self.img = self.walking_right[self.frame_index]
 
         # create the window
-        self.window.geometry('64x64+{x}+0'.format(x=str(self.x)))
+        self.window.geometry(
+            "{w}x{h}+{x}+0".format(
+                x=str(self.x),
+                w=self.walking_right[0].width(),
+                h=self.walking_right[0].height(),
+            )
+        )
         # add the image to our label
         self.label.configure(image=self.img)
         # give window to geometry manager (so it will appear)
@@ -64,5 +83,6 @@ class pet():
 
         # call update after 10ms
         self.window.after(10, self.update)
+
 
 pet()
